e1444232b7fcfe61bfcd33b96087a81cffdf6111
Pascal <github@unrundead.eu>
2019-06-08 15:52:59 +0200

    Added Window PopUp/NewPatient

---
+++ PopUp/NewPatient.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace MoS.PopUp {
+    /// <summary>
+    /// Interaction logic for NewPatient.xaml
+    /// </summary>
+    public partial class NewPatient : Window {
+        public NewPatient() {
+            InitializeComponent();
+        }
+
+        public Objects.Patient Patient;
+
+        private void BtnAddPatient_Click(object sender, RoutedEventArgs e) {
+            Patient = new Objects.Patient() {
+                Firstname = TxtBxFirstname.Text,
+                Lastname = TxtBxLastname.Text,
+                Street = TxtBxStreet.Text,
+                Town = TxtBxTown.Text,
+                Birthday = TxtBxDateOfBirth.Text,
+                Phone = TxtBxPhone.Text,
+                HealthInsurance = TxtBxHealthInsurance.Text,
+                InsuranceNumber = TxtBxInsuranceNumber.Text
+            };
+
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void BtnDiscard_Click(object sender, RoutedEventArgs e) => this.Close();
+    }
+}
