06034687c1724f73797e27cd598585a2f59e508b
Pascal <github@unrundead.eu>
2019-06-08 15:37:11 +0200

    Added Class Classes/Crypto

---
+++ Classes/Crypto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MoS {
+    public static class Crypto {
+        #region SHA256
+        private static String ByteArrayToHexString(byte[] ba) {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat("{0:x2}", b);
+
+            return hex.ToString();
+        }
+
+        public static String CreateSalt(int length) {
+            var rng = new RNGCryptoServiceProvider();
+            var buff = new byte[length];
+            rng.GetBytes(buff);
+
+            return Convert.ToBase64String(buff);
+        }
+
+        public static String HashToSHA256(string password, string salt) {
+            byte[] bytes = Encoding.UTF8.GetBytes(password + salt);
+            SHA256Managed sha256hashstring = new SHA256Managed();
+
+            byte[] hash = sha256hashstring.ComputeHash(bytes);
+
+            return ByteArrayToHexString(hash);
+        }
+        #endregion
+    }
+}
