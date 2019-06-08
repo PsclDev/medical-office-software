ec6671b624fe0c33443f36ed0f0ea668d89d20ac
Pascal <github@unrundead.eu>
2019-06-08 15:52:49 +0200

    Added Window PopUp/NewEmployee

---
+++ PopUp/NewEmployee.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace MoS.PopUp {
+    /// <summary>
+    /// Interaction logic for NewEmployee.xaml
+    /// </summary>
+    public partial class NewEmployee : Window {
+        public NewEmployee() {
+            InitializeComponent();
+        }
+
+        public Objects.Employee Employee;
+
+        private void BtnDiscard_Click(object sender, RoutedEventArgs e) => this.Close();
+
+        private void BtnAddEmployee_Click(object sender, RoutedEventArgs e) {
+            Employee = new Objects.Employee() {
+                Name = TxtBxUsername.Text,
+                Role = ComboboxPermission.Text
+            };
+
+            this.DialogResult = true;
+            this.Close();
+        }
+    }
+}
