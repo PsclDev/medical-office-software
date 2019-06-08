ef784e52978a3ab3925e6d7454474b49e8d244f8
Pascal <github@unrundead.eu>
2019-06-08 15:32:15 +0200

    Added Class Properties/loggedOnUser

---
+++ Properties/loggedOnUser.cs
@@ -0,0 +1,18 @@
+﻿namespace MoS.Properties {
+    public static class loggedOnUser {
+        public static string Username { get; set; }
+        public static string Permission { get; set; }
+
+        public static void EnableAutoLogin(string Username, string Password) {
+            Settings.Default.AutoLogin = true;
+            Settings.Default.AutoLogin_Username = Username;
+            Settings.Default.AutoLogin_Password = Password;
+        }
+
+        public static void DisableAutoLogin() {
+            Settings.Default.AutoLogin = false;
+            Settings.Default.AutoLogin_Username = "";
+            Settings.Default.AutoLogin_Password = "";
+        }
+    }
+}
