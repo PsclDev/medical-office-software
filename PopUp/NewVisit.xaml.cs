a1e6d2dae984faa7eec1bccee163947a00c119ae
Pascal <github@unrundead.eu>
2019-06-08 15:53:05 +0200

    Added Window PopUp/NewVisit

---
+++ PopUp/NewVisit.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace MoS.PopUp {
+    /// <summary>
+    /// Interaction logic for NewVisit.xaml
+    /// </summary>
+    public partial class NewVisit : Window {
+        public NewVisit() {
+            InitializeComponent();
+        }
+
+        public Objects.Visit Visit;
+
+        private void BtnDiscard_Click(object sender, RoutedEventArgs e) {
+            this.Close();
+        }
+
+        private void BtnAddVisit_Click(object sender, RoutedEventArgs e) {
+            Visit = new Objects.Visit() {
+                WrittenSickFromTo = TxtBxSickFromTo.Text,
+                WrittenBy = TxtBxWrittenBy.Text,
+                Result = TxtBxResult.Text,
+                Medication = TxtBxMedication.Text
+            };
+
+            this.DialogResult = true;
+            this.Close();
+        }
+    }
+}
