4859086dbf13718f5a8653f35d4050e32c83f445
Pascal <github@unrundead.eu>
2019-06-08 15:42:40 +0200

    Added Class Objects/Visit

---
+++ Objects/Visit.cs
@@ -0,0 +1,15 @@
+﻿namespace MoS.Objects {
+    public class Visit {
+        public int Id { get; set; }
+        public int PatientID { get; set; }
+        public string Date { get; set; }
+        public string WrittenSickFromTo { get; set; }
+        public string WrittenBy { get; set; }
+        public string Result { get; set; }
+        public string Medication { get; set; }
+
+        public override string ToString() {
+            return $"{Date}";
+        }
+    }
+}
