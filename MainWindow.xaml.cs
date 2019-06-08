49035a4aca160a94f1deb4d6591530cf7cc6aecc
Pascal <github@unrundead.eu>
2019-06-08 16:08:12 +0200

    Designed Window Mos/MainWindow

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MoS {
     /// <summary>
@@ -20,6 +9,60 @@ namespace MoS {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            Properties.AppContent.Load();
+            Log.Trace("Initalize MainWindow");
+            GridMenuList.Children.Add(new UserControls.Menu());
         }
+
+        #region Variables
+        public bool MenuOpen = false;
+        public UserControl UC_PatientDetails = new UserControls.PatientDetails();
+        public UserControl UC_PatientList = new UserControls.PatientList();
+        public UserControl UC_UserManagement = new UserControls.EmployeeManagement();
+        public UserControl UC_Settings = new UserControls.Settings();
+        public UserControl UC_Logs = new UserControls.Logs();
+        #endregion
+
+        #region Header
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e) {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
+        }
+
+        private void BtnLogs_Click(object sender, RoutedEventArgs e) {
+            Grid_Main_UC.Children.Clear();
+            Grid_Main_UC.Children.Add(UC_Logs);
+        }
+
+        private void BtnSettings_Click(object sender, RoutedEventArgs e) {
+            Grid_Main_UC.Children.Clear();
+            Grid_Main_UC.Children.Add(UC_Settings);
+        }
+
+        private void BtnPLogout_Click(object sender, RoutedEventArgs e) {
+            Properties.loggedOnUser.DisableAutoLogin();
+            LoginWindow lw = new LoginWindow();
+            lw.Show();
+            this.Close();
+        }
+
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+
+        private void BtnPowerOff_Click(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+        #endregion
+
+        #region Methods
+        public void ButtonOpenMenu_Click(object sender, RoutedEventArgs e) {
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+            MenuOpen = true;
+        }
+
+        public void ButtonCloseMenu_Click(object sender, RoutedEventArgs e) {
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+            MenuOpen = false;
+        }
+        #endregion
     }
 }
