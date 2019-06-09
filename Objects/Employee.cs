7f0ada57068dc5beae554b66b506610658d50990
Pascal <github@unrundead.eu>
2019-06-09 14:45:59 +0200

    Added Property RoleID Objects/Employee

--- Objects/Employee.cs
+++ Objects/Employee.cs
@@ -2,6 +2,7 @@
     public class Employee {
         public string Name { get; set; }
         public string Role { get; set; }
+        public int RoleID { get; set; }
 
         public Employee() {
 
